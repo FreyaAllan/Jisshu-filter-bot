eeff983f7fccf8025ccc7e0894dae17ec7f139ba
SM <119601732+Alexbots1@users.noreply.github.com>
2024-10-09 09:03:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1468,13 +1468,15 @@ async def auto_filter(client, msg, spoll=False , pm_mode = False):
        if offset != "":
         if total_results >= MAX_BTN:
             btn.insert(0,[
-                InlineKeyboardButton("🈲ʟᴀɴɢᴜᴀɢᴇ🈲 ", callback_data=f"languages#{key}#{offset}#{req}"),
-                InlineKeyboardButton("🫧ǫᴜᴀʟɪᴛʏ🫧 ", callback_data=f"qualities#{key}#{offset}#{req}"),
+                #InlineKeyboardButton("🈲ʟᴀɴɢᴜᴀɢᴇ🈲 ", callback_data=f"languages#{key}#{offset}#{req}"),
+                #InlineKeyboardButton("🫧ǫᴜᴀʟɪᴛʏ🫧 ", callback_data=f"qualities#{key}#{offset}#{req}"),
+	        InlineKeyboardButton("✨ɢᴇᴛᴀʟʟ✨", callback_data=batch_link),
 	    ])
             btn.insert(1, [
                 #InlineKeyboardButton("📥 𝗦𝗲𝗻𝗱 𝗔𝗹𝗹 𝗙𝗶𝗹𝗲𝘀 📥", callback_data=batch_link),
-                InlineKeyboardButton("🗓ꜱᴇᴀꜱᴏɴ🗓", callback_data=f"seasons#{key}#{offset}#{req}"),
-                InlineKeyboardButton("✨ɢᴇᴛᴀʟʟ✨", callback_data=batch_link),
+               InlineKeyboardButton("🫧ǫᴜᴀʟɪᴛʏ🫧 ", callback_data=f"qualities#{key}#{offset}#{req}"),
+	       InlineKeyboardButton("🗓ꜱᴇᴀꜱᴏɴ🗓", callback_data=f"seasons#{key}#{offset}#{req}"),
+                #InlineKeyboardButton("✨ɢᴇᴛᴀʟʟ✨", callback_data=batch_link),
 	    ])  
         else:
             btn.insert(0,[
@@ -1485,19 +1487,19 @@ async def auto_filter(client, msg, spoll=False , pm_mode = False):
             btn.insert(1,[
 		#InlineKeyboardButton("📥 𝗦𝗲𝗻𝗱 𝗔𝗹𝗹 𝗙𝗶𝗹𝗲𝘀 📥", callback_data=batch_link),
                 InlineKeyboardButton("🚸 ɴᴏ ᴍᴏʀᴇ ᴘᴀɢᴇs 🚸", user_id=ADMINS[0]),
-                InlineKeyboardButton("✨ɢᴇᴛᴀʟʟ✨", callback_data=batch_link),
+                #InlineKeyboardButton("✨ɢᴇᴛᴀʟʟ✨", callback_data=batch_link),
 	    ])	
     else:
         btn.insert(0,[
             #InlineKeyboardButton("📥 𝗦𝗲𝗻𝗱 𝗔𝗹𝗹 𝗙𝗶𝗹𝗲𝘀 📥", callback_data=batch_link),
-            InlineKeyboardButton("🚸 ɴᴏ ᴍᴏʀᴇ ᴘᴀɢᴇs 🚸", user_id=ADMINS[0]),
+            #InlineKeyboardButton("🚸 ɴᴏ ᴍᴏʀᴇ ᴘᴀɢᴇs 🚸", user_id=ADMINS[0]),
 	    InlineKeyboardButton("✨ɢᴇᴛᴀʟʟ✨", callback_data=batch_link),
 	])
 
         btn.insert(1,[
 	    #InlineKeyboardButton("📥 𝗦𝗲𝗻𝗱 𝗔𝗹𝗹 𝗙𝗶𝗹𝗲𝘀 📥", callback_data=batch_link),
             InlineKeyboardButton("🚸 ɴᴏ ᴍᴏʀᴇ ᴘᴀɢᴇs 🚸", user_id=ADMINS[0]),
-            InlineKeyboardButton("✨ɢᴇᴛᴀʟʟ✨", callback_data=batch_link),
+            #InlineKeyboardButton("✨ɢᴇᴛᴀʟʟ✨", callback_data=batch_link),
 	])
                              
     if spoll:
